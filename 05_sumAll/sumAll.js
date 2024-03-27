f3be3914687526de72f9c5d4fed2c128ffd3ccd8
Deyan Dyankov <deyandyankov23@yahoo.com>
2024-03-27 17:04:25 +0100

    05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(firstInt, secondInt) {
+    if (!Number.isInteger(firstInt) || !Number.isInteger(secondInt))
+        return "ERROR"
+    if (firstInt < 0 || secondInt < 0)
+        return "ERROR"
+    if (firstInt > secondInt)
+        [firstInt, secondInt] = [secondInt, firstInt]
+    let sum = 0;
+    for (let first = firstInt; first <= secondInt; first++)
+        sum += first;
+    return sum;
 };
 
 // Do not edit below this line
